ad979d62b49ecdacb3e5d22c271d2c5e0d42848b
Mahendra@sam123 <raki020152>
2026-04-04 04:20:27 +0530

    Main class logger statement

--- src/main/java/com/jsp/DspringJenkinsIntegrationApplication.java
+++ src/main/java/com/jsp/DspringJenkinsIntegrationApplication.java
@@ -20,7 +20,7 @@ public class DspringJenkinsIntegrationApplication {
 	public static void main(String[] args) {
 
 		logger.info("Application executed...");
-
+		logger.info("Main class executing...");
 		SpringApplication.run(DspringJenkinsIntegrationApplication.class, args);
 	}
 }
